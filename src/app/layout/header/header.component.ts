0dbf842f9c679360c37d75eca61dafca989e3fc2
Altin <altinmorina2002@gmail.com>
2024-08-09 22:31:55 +0200

    feat: add content to the header

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,12 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output, ViewChild } from '@angular/core';
+import { SharedModule } from '../../shared/shared.module';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [SharedModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
-export class HeaderComponent {
-
-}
+export class HeaderComponent {}

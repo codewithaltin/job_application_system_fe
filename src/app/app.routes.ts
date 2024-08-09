0fc69dd8614c9614038a5d14e81c4042befee5ea
Altin <altinmorina2002@gmail.com>
2024-08-09 22:09:44 +0200

    feat: installed mat angular and added a shared module for easy import of components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,16 @@
 import { Routes } from '@angular/router';
 import { LayoutComponent } from './layout/layout.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: LayoutComponent,
-    children: [],
+    children: [
+      { path: '', component: HomeComponent },
+      { path: 'dashboard', component: DashboardComponent },
+    ],
   },
   { path: '**', redirectTo: '', pathMatch: 'full' },
 ];

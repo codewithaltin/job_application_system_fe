f6123d9645fee720ffee40511298da75135671a5
Altin <altinmorina2002@gmail.com>
2024-09-05 19:28:57 +0200

    add create-job-listing component

---
+++ src/app/components/create-job-listing/create-job-listing.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-job-listing',
+  standalone: true,
+  imports: [],
+  templateUrl: './create-job-listing.component.html',
+  styleUrl: './create-job-listing.component.scss'
+})
+export class CreateJobListingComponent {
+
+}
